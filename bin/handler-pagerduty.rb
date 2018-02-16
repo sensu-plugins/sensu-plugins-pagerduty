72ef1aa294d9323d52ea506484ae395eaa428dad
Scott <scott@internaught.io>
2018-02-16 09:17:17 -0800

    updated elsif to do some double checking

--- bin/handler-pagerduty.rb
+++ bin/handler-pagerduty.rb
@@ -49,7 +49,7 @@ class PagerdutyHandler < Sensu::Handler
     @api_key ||=
       if @event['check']['pager_team']
         settings[json_config][@event['check']['pager_team']]['api_key']
-      elsif settings[json_config][@event['client']['pager_team']].nil?
+      elsif @event['client']['pager_team'] && settings[json_config][@event['client']['pager_team']].nil?
         puts "you configured your client to use a pager team of #{@event['client']['pager_team']} but it was nil, falling back to default key"
         settings[json_config]['api_key']
       elsif @event['client']['pager_team']
